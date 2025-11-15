40170aa86a5a8de6557d6b5c3befdd313199d2c7
Wadday hassan <iwadday@gmail.com>
2025-11-16 04:09:49 +0500

    add "Wue" page with initial template and assets

---
+++ resources/js/pages/Wue.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-white flex h-screen w-screen items-center justify-center">
+        <div>
+            <img src="/images/wuemv.png" alt="Wue.mv" class="w-64" />
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Wue', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
