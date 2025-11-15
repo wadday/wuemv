92ecba03162a656622ed2a514fcdfc26af322882
Wadday hassan <iwadday@gmail.com>
2025-11-16 04:10:15 +0500

    build

--- public/build/assets/Spinner.vue_vue_type_script_setup_true_lang-CGhmJc9y.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./utils-TfETDaq2.js";import{L as r}from"./loader-circle-BRvYWPOO.js";import{d as n,f as c,u as s,n as t,o as l}from"./app-BZKtaPp6.js";const _=n({__name:"Spinner",props:{class:{}},setup(a){const e=a;return(i,p)=>(l(),c(s(r),{role:"status","aria-label":"Loading",class:t(s(o)("size-4 animate-spin",e.class))},null,8,["class"]))}});export{_};
